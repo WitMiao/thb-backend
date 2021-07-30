b108978ec8c6231d84c80420f9c0d96eaf3ba89d
WitMiao <454690789@qq.com>
2021-07-30 16:14:54 +0800

    public up

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ limit: '500mb', extended: false }));
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.static(path.join(__dirname, "/public")));
 app.use(
   session({
     resave: false,
